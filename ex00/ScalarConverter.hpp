040a32635e3c36f2bc9036c7c19f7dcce1b29266
Anthony Grimaldo Ramos <hernangrim.10@gmail.com>
2025-01-28 20:17:33 +0100

    entiendo poc el ex00

---
+++ ex00/ScalarConverter.hpp
@@ -0,0 +1,10 @@
+# pragma once
+# include <iostream>
+
+class ScalarConverter
+{
+	private:
+		// Aqui van a ir las funciones privadas, ya que no se pueden acceder desde fuera de la clase
+	public:
+
+};

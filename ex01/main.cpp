73523e07043acfad81d9a78775d80a837f25899a
Anthony Grimaldo Ramos <hernangrim.10@gmail.com>
2025-01-30 17:00:22 +0100

    casi terminado ex01

--- ex01/main.cpp
+++ ex01/main.cpp
@@ -1,8 +1,17 @@
-# include "main.hpp"
+# include "Serializer.hpp"
 
 int main()
 {
-	std::string menssage = "Hello Wordl!";
+	s_data* ptr = new s_data;
 
-	std::cout << menssage << std::endl;
+	ptr->nbr = 42;
+
+	uintptr_t raw = Serializer::serializer(ptr);
+	std::cout << "\033[92m\nSerialized pointer: \033[0m" << raw << std::endl;
+
+	s_data* newPtr = Serializer::deseralize(raw);
+	std::cout << "\033[92mDeserialized pointer points to value: \033[0m" << newPtr->nbr << "\n" << std::endl;
+
+	delete ptr;
+	return 0;
 }
